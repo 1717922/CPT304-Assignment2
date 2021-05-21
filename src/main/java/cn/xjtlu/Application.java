964a61fe0fa317670d578c9f7edb60c71d26a870
YCKJ2885 <shenjiale@cloudwalk.cn>
2021-05-21 20:07:24 +0800

    initial commit

---
+++ src/main/java/cn/xjtlu/Application.java
@@ -0,0 +1,18 @@
+package cn.xjtlu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author kenny
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
+
+
